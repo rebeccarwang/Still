3c4c879ff2159dc2ba761623bbe03d77083e7f19
rebeccarwang <rebeccaw48@yahoo.com>
2025-05-14 15:40:17 -0700

    connection to Neon db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
+const {Pool} = require('pg');
+
+// connect to PostgreSQL database
+const pool = new Pool({
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  ssl: {rejectUnauthorized: false}
+});
+
 
 app.use(cors());
+app.user(express.json());
 
 app.get('/', (req, res) => {
   res.send('Testing')
@@ -11,4 +25,3 @@ app.get('/', (req, res) => {
 app.listen(8080, () => {
   console.log('server listening on port 8080');
 })
-
