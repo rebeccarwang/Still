943d4647d3f9cee7e87f40f6ac6e35f804c9ff00
rebeccarwang <rebeccaw48@yahoo.com>
2025-05-29 15:02:56 -0700

    feat: sentiment analysis with Hugging Face and FastAPI

---
+++ sentiment-service/sentiment.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from transformers import pipeline
+
+app = FastAPI()
+
+classifier = pipeline('sentiment-analysis', model='distilbert-base-uncased-finetuned-sst-2-english')
+
+# checks field of journalText has a string input when SentimentAnalysis is passed in as parameter
+class SentimentAnalysis(BaseModel):
+  journalText: str
+
+# analyzes sentiment of journal entry text
+@app.post('/api/sentiment')
+def analyze_sentiment(journal_entry: SentimentAnalysis):
+  user_text = journal_entry.journalText
+  try:
+    res = classifier(user_text)[0]
+    label = res['label']
+
+    # uses confidence score as a proxy for positivity score
+    score = res['score']
+    if label == 'NEGATIVE':
+      score = 1 - score
+    return {'score': score}
+  except Exception as e:
+    raise HTTPException(status_code=500, detail='Sentiment analysis failed. Please try again')
--- server/routes/journal.js
+++ server/routes/journal.js
@@ -13,12 +13,26 @@ router.post('/entries', isAuthenticated, async (req, res) => {
   }
 
   try {
+
+    // get sentiment score for journalEntry text
+    const sentimentRes = await fetch('http://localhost:8000/api/sentiment', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({journalText})
+    });
+    const sentimentResJson = await sentimentRes.json();
+    const sentimentScore = sentimentResJson.score;
+    // console.log('sentiment score:', sentimentScore);
+
     // create new entry in journalEntry table
     const newJournalEntry = await prisma.journalEntry.create({
       data: {
         userId: req.session.userId,
         content: journalText,
-        moodAtTimeOfEntry: req.session.mood
+        moodAtTimeOfEntry: req.session.mood,
+        sentimentScore: sentimentScore
       }
     });
 
