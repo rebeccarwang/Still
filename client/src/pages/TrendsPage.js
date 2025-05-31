7a49e89f65383f2c7c162d4403527a0b6f2dd171
rebeccarwang <rebeccaw48@yahoo.com>
2025-05-31 00:35:41 -0700

    feat: trends frontend

--- client/src/pages/TrendsPage.js
+++ client/src/pages/TrendsPage.js
@@ -1,5 +1,6 @@
 import {useNavigate} from 'react-router-dom';
 import {useState, useEffect} from 'react';
+import {LineChart, XAxis, YAxis, Line, Tooltip} from 'recharts';
 
 
 export default function TrendsPage() {
@@ -7,11 +8,11 @@ export default function TrendsPage() {
   const [loading, setLoading] = useState(true);
   const [serverError, setServerError] = useState('');
   const [trends, setTrends] = useState(null);
+  const mostCommonTags = trends ? mostCommonTagsFrequency(trends.tagFrequencies) : null;
 
   useEffect(() => {
     const fetchTrends = async () => {
       try {
-        console.log('fetching trends');
         const resTrends = await fetch(`http://localhost:8080/api/trends/mood/weekly`, {
           credentials: 'include',
         });
@@ -60,13 +61,48 @@ export default function TrendsPage() {
 
   return (
     <>
-    Trending
-    {!loading && (<>
-    {/* <h3>{JSON.stringify(trends.tagFrequencies)}</h3>
-    <h3>{JSON.stringify(mostCommonTagsFrequency(trends.tagFrequencies))}</h3> */}
-
+    <h1>Trends</h1>
+    {!loading && trends.moodData.length === 0 && (
+      <h3>No moods were input in the past week.</h3>
+    )}
+    {!loading && trends.moodData.length > 0 && (<>
+    <div>
+      <h2>Mood Trends</h2>
+      <LineChart width={400} height={200} data={trends.moodData.map(entry => ({createdAt: new Date(entry.createdAt).getTime(), mood: entry.mood}))}>
+        <XAxis
+          dataKey='createdAt'
+          type='number'
+          domain={['auto', 'auto']}
+          scale='time'
+          label={{value: 'Date', position: 'insideBottom', offset: -5}}
+          tickFormatter={(datetime) => new Date(datetime).toISOString().split('T')[0]}
+        />
+        <YAxis
+          domain={[0, 6]}
+          label={{value: 'Mood', position: 'insideLeft', offset: -5}}
+        />
+        <Tooltip
+          labelFormatter={(datetime) => {
+            const date = new Date(datetime);
+            return date.toLocaleString();
+          }}
+        />
+        <Line type='monotone' dataKey='mood' stroke='#8884d8' />
+      </LineChart>
+    </div>
+    </>)}
+    <h2>Tag Trends</h2>
+    {!loading && !mostCommonTags && (
+      <h3>No tags were input in the past week.</h3>
+    )}
+    {!loading && mostCommonTags && (
+      <>
+      Your most frequent tags this week: {mostCommonTags[0].map(entry => `${entry} `)}
+      <br />
+      tagged {mostCommonTags[1]} time(s) each
     </>)}
     {serverError && <p style={{ color: 'red' }}>{serverError}</p>}
+    <br />
     <button onClick={() => navigate(-1)}>Back</button>
     </>
   )
