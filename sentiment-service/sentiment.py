73e08abb2befbd5e920c76a2e8e10e9a2bf59ef5
rebeccarwang <rebeccaw48@yahoo.com>
2025-06-08 00:12:40 -0700

    sentiment-service: optimize Dockerfile for memory; add rate limiting per IP

--- sentiment-service/sentiment.py
+++ sentiment-service/sentiment.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from transformers import pipeline
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+app.state.limiter = limiter
+
+# error message if user exceeds limit
+@app.exception_handler(RateLimitExceeded)
+async def rate_limit_handler(request: Request, exc: RateLimitExceeded):
+  return JSONResponse(
+    status_code=429, content={'error': 'Maximum allowable number of requests exceeded'}
+  )
 
 classifier = pipeline('sentiment-analysis', model='distilbert-base-uncased-finetuned-sst-2-english')
 
@@ -11,12 +24,14 @@ class SentimentAnalysis(BaseModel):
   journalText: str
 
 @app.get('/')
-def root():
+@limiter.limit('5/hour')
+def root(request: Request):
   return {'status': 'ok'}
 
 # analyzes sentiment of journal entry text
 @app.post('/api/sentiment')
-def analyze_sentiment(journal_entry: SentimentAnalysis):
+@limiter.limit('10/hour')
+def analyze_sentiment(journal_entry: SentimentAnalysis, request: Request):
   user_text = journal_entry.journalText
   try:
     res = classifier(user_text)[0]
