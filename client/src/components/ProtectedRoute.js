17fd61454dd62e5d145fb314c920b0c1ff6f62af
rebeccarwang <rebeccaw48@yahoo.com>
2025-06-21 21:10:21 -0700

    fix: delay upon app load

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -2,7 +2,14 @@ import {Navigate} from 'react-router-dom';
 import {useAuth} from '../hooks/AuthContext';
 
 const ProtectedRoute = ({children}) => {
-  const {user} = useAuth();
+  const {user, loading} = useAuth();
+  if (loading) {
+    return (<>
+    <div className='bg-white bg-opacity-70 flex items-center justify-center'>
+    <div className='absolute p-8 lg:p-12 text-med-orange text-lg lg:text-2xl italic whitespace-nowrap'>Loading...</div>
+    </div>
+    </>)
+  }
   if (!user) {
     return <Navigate to='/' />;
   }
--- client/src/hooks/AuthContext.js
+++ client/src/hooks/AuthContext.js
@@ -47,7 +47,7 @@ export const AuthProvider = ({children}) => {
 
   return (
     <AuthContext.Provider value = {{user, login, logout, loading}}>
-      {!loading && children}
+      {children}
     </AuthContext.Provider>
   )
 }
